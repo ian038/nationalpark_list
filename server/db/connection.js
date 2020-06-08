28dc55cced1c987d8ba6d379e0fe634c4425663f
ian038 <ianphua3@gmail.com>
2020-06-08 17:52:41 -0400

    Refactor

--- server/db/connection.js
+++ server/db/connection.js
@@ -15,7 +15,19 @@ const mysqlConnection = mysql.createConnection({
         console.log('DB opened')
     } else {
         console.log('Error' + err)
+        setTimeout(handleDisconnect, 2000)
     }
   })
+
+  mysqlConnection.on('Error', (err) => {
+    console.log('db error', err)
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect()
+    } else {
+      throw err
+    }
+  })
+
+  handleDisconnect()
 
 module.exports = mysqlConnection
