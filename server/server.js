9d64aa6d4e431203fcf34d2733407800c2ffa260
ian038 <ianphua3@gmail.com>
2020-06-08 17:25:56 -0400

    Refactor

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ const port = process.env.PORT || 5000
 if(process.env.NODE_ENV === 'production') {
   // static folder
   app.use(express.static('client/build'))
+  console.log(__dirname)
   // handle SPA
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
