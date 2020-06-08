c4f8a8e7f54a439905e6f603f682facc22581bc2
ian038 <ianphua3@gmail.com>
2020-06-08 18:08:58 -0400

    Refactor

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,30 +2,34 @@ const mysql = require('mysql')
 require('dotenv').config()
 
 // Database
-const mysqlConnection = mysql.createPool({
+const mysqlConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     multipleStatements: true
   })
-  
-  mysqlConnection.connect((err) => {
-    if(!err) {
-        console.log('DB opened')
-    } else {
-        console.log('Error' + err)
-        // setTimeout(handleDisconnect, 2000)
-    }
-  })
 
-  // mysqlConnection.on('Error', (err) => {
-  //   console.log('db error', err)
-  //   if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-  //     handleDisconnect()
-  //   } else {
-  //     throw err
-  //   }
-  // })
+  function handleDisconnect() {
+    mysqlConnection.connect((err) => {
+      if(!err) {
+          console.log('DB opened')
+      } else {
+          console.log('Error' + err)
+          setTimeout(handleDisconnect, 2000)
+      }
+    })
+  
+    mysqlConnection.on('Error', (err) => {
+      console.log('db error', err)
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        handleDisconnect()
+      } else {
+        throw err
+      }
+    })
+  }
+
+  handleDisconnect()
 
 module.exports = mysqlConnection
