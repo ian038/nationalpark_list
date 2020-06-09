49421b8db3486ef1567cfd82d74a91f521a4e6d6
ian038 <ianphua3@gmail.com>
2020-06-08 21:51:59 -0400

    Fixing to deploy

--- server/db/connection.js
+++ server/db/connection.js
@@ -14,7 +14,7 @@ const mysqlConnection = mysql.createConnection({
 
   function handleDisconnect() {
     // recreate connection
-    connection = mysql.createConnection({
+    connection = mysql.createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
