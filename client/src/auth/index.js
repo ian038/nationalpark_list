fcb0bd088236b167c7db2d48fe3505f13e181e98
ian038 <ianphua3@gmail.com>
2020-06-09 10:22:27 -0400

    Prepare for deployment

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,9 +1,10 @@
+import { SERVER } from '../config'
 import axios from 'axios'
 
 export const signup = user => {
     return axios({
             method: 'post',
-            url:`api/auth/signup`,
+            url:`/api/auth/signup`,
             headers: {
                 Accept: "*/*",
                 "Content-Type": "application/json"
@@ -15,7 +16,7 @@ export const signup = user => {
 export const signin = user => {
     return axios({
             method: 'post',
-            url:`api/auth/signin`,
+            url:`/api/auth/signin`,
             headers: {
                 Accept: "*/*",
                 "Content-Type": "application/json"
@@ -28,7 +29,7 @@ export const signout = next => {
     if(typeof window !== 'undefined') {
         localStorage.removeItem('jwt')
         next()
-        return axios.get(`api/auth/signout`)
+        return axios.get(`/api/auth/signout`)
         .then(res => console.log(res))
         .catch(error => console.log(error))
     }
